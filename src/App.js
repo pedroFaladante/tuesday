b346307686907a00f7c49867b5fb625aabf96de4
Your Name <pcontato19@gmail.com>
2022-04-02 16:15:32 +0100

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import TicketPage from './pages/TicketPage'
+import Nav from './components/Nav'
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={{ Dashboard }} />
+          <Route path="/ticket" element={{ TicketPage }} />
+
+
+        </Routes>
+      </BrowserRouter>
+      <div>
+        Home
+      </div>
 
     </div>
   );
--- src/pages/TicketPage.jsx
+++ src/pages/TicketPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
  function TicketPage() {
   return (
-    <div>TicketPage</div>
+    <div>TicketPage Bro</div>
   )
 }
 
